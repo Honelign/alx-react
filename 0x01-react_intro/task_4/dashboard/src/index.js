0dc1cc30c04edeca5e10de65cb24be080a20e107
Honelign <honelignselamu2@gmail.com>
2023-02-08 15:01:23 +0300

    inline styling

--- 0x01-react_intro/task_4/dashboard/src/index.js
+++ 0x01-react_intro/task_4/dashboard/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -18,4 +18,28 @@ rootNotifications.render(
   </React.StrictMode>
 );
 
+reportWebVitals(); */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Notifications from "./Notifications";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
